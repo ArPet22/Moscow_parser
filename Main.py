02cf9db5630a77f58d94b5ab5c379fc8e983a0c4
ArPet <ariadnapetko@gmail.com>
2025-07-07 20:53:05 +0300

    init

--- Main.py
+++ Main.py
@@ -1,1 +1,8 @@
 from telethon import TelegramClient
+from conf import api_id, api_hash
+client = TelegramClient("sesion", api_id, api_hash)
+async def main():
+    me = await client.get_me()
+    print(me.stringify())
+with client:
+    client.loop.run_until_complete(main())
